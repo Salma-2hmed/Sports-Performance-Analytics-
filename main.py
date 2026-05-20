edca36a165b8a26f24b8aa3cae0e2bd0575f09eb
Salma Ahmed <you@example.com>
2026-05-20 14:14:13 +0300

    new commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from api.app import create_app
 app = create_app()
 print("=" * 60)
-print("  Sports Performance Analytics API - Project 48")
+print(" Sports Performance Analytics API")
 print("=" * 60)
 app.run(debug=True, port=5000)
